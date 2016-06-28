72e9bcb49dd315b74f707974a34b4c00b7cc0420
bneiluj <julien.bouteloup@gmail.com>
2016-06-28 19:13:27 +0100

    Add chart for individual weather component

---
+++ src/components/chart.js
@@ -0,0 +1,28 @@
+// it's a component not a container because the state is directly pass through weather_list
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
+// class or function based component ?
+// we don't need state or extra fancy stuff
+// so let's use just a function component
+export default (props) => {
+  return (
+    <div>
+      <Sparklines
+        height={120}
+        width={180}
+        data={props.data}
+        >
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>
+        {average(props.data)}
+      </div>
+    </div>
+  )
+}
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 // export default class WeatherList extends Component {
 // we not exporting anymore as we are connecting to connect
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
+    console.log("temps: ", temps);
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td>
+          {name}
+        </td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={pressures} color="green" /></td>
+        <td><Chart data={humidities} color="black" /></td>
       </tr>
     )
   }
