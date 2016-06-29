905cd6c9a1bcabbbb5493daf10a30063fec9736b
bneiluj <julien.bouteloup@gmail.com>
2016-06-29 12:09:46 +0100

    Connect google map to weather container

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 // export default class WeatherList extends Component {
 // we not exporting anymore as we are connecting to connect
@@ -10,13 +11,15 @@ class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    // const lon = cityData.city.coord.lon;
+    // const lat = cityData.city.coord.lat;
+    // let's use destructuring es6 behavior
+    const { lon, lat } = cityData.city.coord;
 
     console.log("temps: ", temps);
     return (
       <tr key={name}>
-        <td>
-          {name}
-        </td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="°C"/></td>
         <td><Chart data={pressures} color="green" units="hPa"/></td>
         <td><Chart data={humidities} color="black" units="%"/></td>
