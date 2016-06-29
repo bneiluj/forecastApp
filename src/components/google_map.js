29f78f6de285514d92dff91c507b6d9275393639
bneiluj <julien.bouteloup@gmail.com>
2016-06-29 12:09:12 +0100

    Finish google map implemetation

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,18 +1,18 @@
 // We are not going to use markets or etc
 // so just a functional component
-import 'React' from 'react';
-import {GoogleMapLoader, GoogleMap } from "react-google-maps";
+import React from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
 export default (props) => {
+  console.log("props.lat: ", props.lat);
+  console.log("props.lon: ", props.lon);
   return (
     <GoogleMapLoader
-      containerElement={ < div style={{height: '100%'}} /> }
+      containerElement={ <div style={{height: '100%'}} /> }
       googleMapElement={
-        <GoogleMap
-          defaultZoom={12}
-          defaultCenter={{ lat: props.lat, lng: props.lon }}
-        >
+        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}}>
+        </GoogleMap>
       }
-    >
-  )
+      />
+  );
 }
